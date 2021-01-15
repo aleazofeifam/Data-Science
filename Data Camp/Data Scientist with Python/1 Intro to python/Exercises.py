8fee1149c95483460ab86a1e54cfbbe9efe7bf1b
Alejandro Azofeifa <aleazofeifam@gmail.com>
2021-01-15 08:59:06 -0600

    Added new folder
    Included my learning material on DataCamp

---
+++ Data Camp/Data Scientist with Python/1 Intro to python/Exercises.py
@@ -0,0 +1,5 @@
+
+
+# List manipulation
+## Changing list elements
+
---
+++ Data Camp/Data Scientist with Python/1 Intro to python/main.py
@@ -0,0 +1,7 @@
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Correct the bathroom area
+
+
+# Change "living room" to "chill zone"
