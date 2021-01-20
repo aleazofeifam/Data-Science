735fea7ade2b0844c69b363fb6a44216cee77b79
Alejandro Azofeifa <aleazofeifam@gmail.com>
2021-01-20 11:39:57 -0600

    Re-ordered file content
    Split file content

--- Unit 1 - Getting started with the data science tools/Module 2 - Numpy and Pandas/Manejar XML.py
+++ Unit 1 - Getting started with the data science tools/Module 2 - Numpy and Pandas/Manejar XML.py
@@ -1,46 +1,4 @@
 
-
-
-
-##MANEJO DE ARCHIVOS JSON
-##Cargar json y leerlos en consola
-import json
-with open('mercados.json', 'rb') as InformacionDeMercados:
-    mercados =json.load(InformacionDeMercados)
-mercados
-
-##Lectura del archivo
-print("******Imprimir cada key******")
-print(mercados.keys())
-
-print("******Imprimir toda la info de la llave en una sola linea******")
-print(mercados['IBEX_MEDIUM_CAP'])
-
-print("******Imprimir los arrays de cada key de manera ordenada******")
-for Arreglos in mercados['IBEX_MEDIUM_CAP']:
-    print(Arreglos)
-
-print("******Imprimir los values de cada key de manera ordenada******")
-for Arreglos in mercados['IBEX_MEDIUM_CAP']:
-    for Valores in Arreglos.keys():
-        print(Arreglos[Valores])
-
-##Escritura en el archivo
-NuevoMercado = [
-    {'MSFT': [1, 2, 3, 4, 5]},
-    {'APPL': [5, 4, 3, 2, 1]}
-]
-mercados.update({'NASDAQ' : NuevoMercado})
-
-print("La lista completa de la llaves, incluyendo los nuevos")
-print(mercados.keys())
-
-with open('Mercados_update.json', 'w') as outfile:
-    json.dump(mercados, outfile)
-
-
-##MANEJO DE ARCHIVOS XML
-
 ##Cargar el xml
 import xml.dom.minidom
 raw_xml = xml.dom.minidom.parse('books.xml')
